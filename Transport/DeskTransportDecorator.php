95242a94dfbfe51b35d64d7ec724aa9443abcd57
Gabe Blair <gabe@castironcoding.com>
2017-08-23 15:52:39 -0700

    [WIP] Add Desk params automatically

--- Transport/DeskTransportDecorator.php
+++ Transport/DeskTransportDecorator.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace CristianPontes\ZohoCRMClient\Transport;
+use CristianPontes\ZohoCRMClient\Exception\Exception;
 
 /**
  * Class DeskTransportDecorator
@@ -8,15 +9,42 @@ namespace CristianPontes\ZohoCRMClient\Transport;
  */
 class DeskTransportDecorator extends XmlDataTransportDecorator
 {
+    protected $portalName;
+    protected $departmentName;
+
     /**
      * DeskTransportDecorator constructor.
      * @param Transport $portalName
      * @param $departmentName
      * @param Transport $transport
+     * @throws Exception
      */
     public function __construct($portalName, $departmentName, Transport $transport)
     {
+        if (!$portalName) {
+            throw new Exception('Please provide a portal name');
+        }
+
+        if (!$departmentName) {
+            throw new Exception('Please provide a department name');
+        }
+
+        $this->departmentName = $departmentName;
+        $this->portalName = $portalName;
 
         parent::__construct($transport);
+    }
+
+    /**
+     * @param string $module
+     * @param string $method
+     * @param array $paramList
+     * @return string
+     */
+    public function call($module, $method, array $paramList)
+    {
+        $paramList['portal'] = $this->portalName;
+        $paramList['department'] = $this->departmentName;
+        return parent::call($module, $method, $paramList);
     }
 }
